e2f6604cfbc46469603aa6742c764247b67208e1
highdt <highdt@163.com>
2017-03-04 16:54:48 +0800

    first prototype
    + add shooter
    + add collision check
    + add score board

---
+++ Assets/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public Vector3 m_velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        transform.Translate(m_velocity);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger hit " + collision.gameObject.name);
+
+        GameObject gameObj = GameObject.Find("MainGame");
+        MainGame mainGame = gameObj.GetComponent<MainGame>();
+        mainGame.OnBulletHit(this.gameObject, collision.gameObject);
+
+        Destroy(this.gameObject);
+    }
+}
